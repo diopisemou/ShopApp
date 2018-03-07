fd823cb0cab682360b6364c5eebdad8ccb0456dd
Mouhamadou Tidiane El Bachir Diop <diopisemou@gmail.com>
2018-03-07 17:52:00 +0100

    Implemented Activity Indicator

--- ShopApp/ShopApp/MainPage.xaml.cs
+++ ShopApp/ShopApp/MainPage.xaml.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Microsoft.Win32.SafeHandles;
 using ShopApp.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
+using Timer = System.Timers.Timer;
 
 namespace ShopApp
 {
     public partial class MainPage : ContentPage
     {
+        private int elemindex;
+        private Grid elemgrids;
         public MainPage()
         {
             InitializeComponent();
@@ -42,7 +49,7 @@ namespace ShopApp
                 ClickGestureRecognizer  clickRecognizer =  new ClickGestureRecognizer();
                 clickRecognizer.Clicked += ClickGestureRecognizer_OnClicked;
                 TapGestureRecognizer tapGesture = new TapGestureRecognizer();
-                tapGesture.Tapped += ClickGestureRecognizer_OnTapped;
+                tapGesture.Tapped += ClickGestureRecognizer_OnTappedAsync;
                 elem.GestureRecognizers.Add(clickRecognizer);
                 elem.GestureRecognizers.Add(tapGesture);
 
@@ -50,8 +57,14 @@ namespace ShopApp
                 this.Resources.TryGetValue("CarouselLabelStyle", out res);
                 Style labsStyle = (Style) res;
                 elem.Style = labsStyle ;
+                elem.FontAttributes = FontAttributes.None;
+                elem.FontSize = 15;
+                elem.Opacity = 0.7;
+                elem.Margin = new Thickness(5);
                 FilterIem.Children.Add(elem);
             }
+
+            elemindex = 0;
         }
 
         public List<ItemPair> ShopItems { get; set; }
@@ -136,7 +149,7 @@ namespace ShopApp
             {
                 var fil = new Filter(SearchBar_Text.Text, 0);
                 List<ItemPair> results = ShopItems.FindAll(
-                    delegate (ItemPair bk) { return bk.Item1.Categorie.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName); }
+                    delegate (ItemPair bk) { return bk.Item1.Name.Contains(fil.FilterName) || bk.Item2.Name.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName); }
                 );
                 if (results.Count != 0)
                 {
@@ -164,7 +177,7 @@ namespace ShopApp
             {
                 var fil = new Filter(SearchBar_Text.Text, 0);
                 List<ItemPair> results = ShopItems.FindAll(
-                    delegate (ItemPair bk) { return bk.Item1.Categorie.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName); }
+                    delegate (ItemPair bk) { return bk.Item1.Name.Contains(fil.FilterName) || bk.Item2.Name.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName) || bk.Item2.Categorie.Contains(fil.FilterName); }
                 );
                 if (results.Count != 0)
                 {
@@ -198,24 +211,29 @@ namespace ShopApp
             public int ColumnID { get; set; }
         }
 
-        private void ClickGestureRecognizer_OnTapped(object sender, EventArgs e)
+        private async void ClickGestureRecognizer_OnTappedAsync(object sender, EventArgs e)
         {
             
-            
-
             Label senderLabel = (Label) sender ;
 
+            ProductList.IsRefreshing = true;
             if (senderLabel!= null)
             {
                 foreach (Label VARIABLE in FilterIem.Children)
                 {
                     if (senderLabel.Text == VARIABLE.Text)
                     {
-                        VARIABLE.FontSize = 0.1*(VARIABLE.FontSize) + VARIABLE.FontSize;
+                        int nbrelement = FilterIem.Children.Count;
+                        double xmilieu = FilterIem.Children.ElementAt(nbrelement/2).X;
+                        double ymilieux = FilterIem.Children.ElementAt(nbrelement / 2).Y;
+                        VARIABLE.ScaleTo(1.1, 1500);
+                        //VARIABLE.TranslateTo(xmilieu, VARIABLE.Y, 1000);
+                        //FilterIem.Children.ElementAt(nbrelement / 2).TranslateTo(-VARIABLE.X, ymilieux, 1000);
+                        //VARIABLE.FontSize = 0.1*(VARIABLE.FontSize) + VARIABLE.FontSize;
                         VARIABLE.Opacity = 1;
                         VARIABLE.TextColor = Color.FromHex("#F9FAFC");
                         VARIABLE.FontAttributes = FontAttributes.Bold;
-                        VARIABLE.Margin = new Thickness(0,-4,0,0);
+                        VARIABLE.Margin = new Thickness(5,-4,5,0);
                     }
                     else
                     {
@@ -226,6 +244,8 @@ namespace ShopApp
                     }
                 }
 
+              
+                
                 var fil = new Filter(senderLabel.Text,0);
                 List<ItemPair> results = ShopItems.FindAll(
                     delegate (ItemPair bk) { return bk.Item1.Categorie.Contains(fil.FilterName); }
@@ -243,20 +263,30 @@ namespace ShopApp
                     ProductList.ItemsSource = noresults;
 
                 }
-
+                //AddProgressDisplay(this, this.ActIndicator);
                 //await DisplayAlert("Item Tapped", "Le Filtre : " + fil.FilterName, "OK");
+
             }
             else
             {
                 DisplayAlert("Label", "Label Tapped", "Ok");
             }
 
+
             //await DisplayAlert("Item Tapped", "An item was tapped."+e.ToString(), "OK");
 
             //Deselect Item
             //((ListView)sender).SelectedItem = null;
 
-            //DisplayAlert("Label", "Label Tapped", "Ok");
+            Timer a = new Timer(500);
+            a.AutoReset = false;
+            a.Start();
+            a.Elapsed += A_Elapsed;
+        }
+
+        private void A_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ProductList.IsRefreshing = false;
         }
 
         private void ClickGestureRecognizer_OnClicked(object sender, EventArgs e)
@@ -273,6 +303,29 @@ namespace ShopApp
             //ItemPair bk = senderFrame.BindingContext as ItemPair;
             shopi = senderFrame.BindingContext as ShopItem;
             DisplayAlert("Frame Tapped ", "Product Name : " + shopi.Name + " Product Category : " + shopi.Categorie, "Ok");
+        }
+
+        public void AddProgressDisplay(ContentPage page,ActivityIndicator activ)
+        {
+            var content = page.Content;
+            var grid = new Grid();
+            grid.Children.Add(content);
+            var gridProgress = new Grid { BackgroundColor = Color.FromHex("#64FFE0B2"), Padding = new Thickness(50) };
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            //gridProgress.SetBinding(VisualElement.IsVisibleProperty, "IsWorking");
+            activ = new ActivityIndicator
+            {
+                IsEnabled = true,
+                IsVisible = true,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                IsRunning = true
+            };
+            gridProgress.Children.Add(activ ,0, 1);
+            grid.Children.Add(gridProgress);
+            this.elemindex = grid.Children.IndexOf(gridProgress);
+            page.Content = grid;
         }
     }
 }
